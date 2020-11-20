2e9cda50103d629ec861943a656f3809c7ff41b1
Akihiro <43564475+awakuwaku@users.noreply.github.com>
2020-11-20 15:21:54 +0900

    Add Ruby on Rails Files 2-7

--- rails/sample_app/config/initializers/application_controller_renderer.rb
+++ rails/sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '3528c86a614bbb8491f9cf6d53be4dcef46fb344686073d769216414723dc0324725de2d2b4f9f4abbef24dc24cfaf6d351751768a8d3f48940968f6fbe63666'
