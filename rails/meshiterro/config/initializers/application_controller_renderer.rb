8e774942dd43d235ab1b2e176369c3c7bd66d740
Akihiro <43564475+awakuwaku@users.noreply.github.com>
2020-11-21 11:45:25 +0900

    Add Ruby on Rails Files 4-3

--- rails/meshiterro/config/initializers/application_controller_renderer.rb
+++ rails/meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'b3bb776bb6f895ba6d419dfd3da65d009a47943849bb518e8bdd7e951221f320060376e2cc614c33df4231f653b2a8b39ca69ac41e1ebad10201eb9303d82907'
