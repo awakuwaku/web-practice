c656847a2849d5b49ca28e25e5d69112cad6b81a
Akihiro <43564475+awakuwaku@users.noreply.github.com>
2020-11-21 13:38:45 +0900

    Add Ruby on Rails Files 4-6

--- rails/meshiterro/app/controllers/application_controller.rb
+++ rails/meshiterro/app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  # 認証対象から除外
+  before_action :authenticate_user!,except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
