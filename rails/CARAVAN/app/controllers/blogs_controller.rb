b806c336017ca63b1ae62130859036d58f26441c
Akihiro <43564475+awakuwaku@users.noreply.github.com>
2020-11-21 10:05:41 +0900

    Add Ruby on Rails Files 3-5

--- rails/CARAVAN/app/controllers/blogs_controller.rb
+++ rails/CARAVAN/app/controllers/blogs_controller.rb
@@ -3,7 +3,9 @@ class BlogsController < ApplicationController
     @blogs = Blog.all
   end
 
-  def show; end
+  def show
+    @blog = Blog.find(params[:id])
+  end
 
   def new
     @blog = Blog.new
@@ -13,6 +15,7 @@ class BlogsController < ApplicationController
     blog = Blog.new(blog_params)
     blog.save
     redirect_to blogs_path
+    redirect_to blog_path(blog.id)
   end
 
   def edit; end
